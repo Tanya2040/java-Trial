d456175a9f9e65ce2048eb3242b320d175ecb70f
Tanya2040 <guptatanya473@gmail.com>
2023-03-21 15:15:38 +0530

    first commit

---
+++ pw1.java
@@ -0,0 +1,11 @@
+Z{
+int x;//instance variable
+public static void main(String w[]){
+Z ob=new Z();
+Z ob1=new Z();
+  ob.x=10;
+  ob1.x=20;
+  System.out.println(ob1.x);
+  System.out.println(ob.x);
+}class
+}
